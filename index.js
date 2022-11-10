89cf84c2cdb529807054404f8cb3fdc6173b88be
jlpbiuma <jpordomingo@iuma.ulpgc.es>
2022-11-10 13:36:08 +0000

    GENERAL: Modified index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ api.use(cors());
 api.use(express.json());
 api.use("/api",routes);
 
-api.listen("3000");
+api.listen(process.env.PORT || 3000);
 
 mongoose.connect(process.env.MONGO_URL,{ dbName: process.env.MONGO_DB})
 
