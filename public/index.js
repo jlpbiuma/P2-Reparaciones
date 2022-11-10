60d22a7314602cc8a4451d651846a45dcfa91940
Joseph <jvp.2703@gmail.com>
2022-11-10 19:42:08 +0000

    README: modified PUBLIC: html and js modified

--- public/index.js
+++ public/index.js
@@ -1,12 +1,32 @@
+const api = axios.create({
+    baseURL: "https://p2-reparaciones-production.up.railway.app/",
+    timeout: 2000
 
-const getAllUsers = async () => {
-    const response = await axios.get()
-    console.log(response);
+});
+
+const login = async () => {
+    const response = await api.post('api/auth/login', {
+        email: "josephadmin@gmail.com",
+        password: "123456789"
+    })
+    return response.token;
 }
 
+let token = login();
+
+const getAllUsers = async () => {
+    const users = await api.get('api/users/?token=' + token)
+    console.log(users)
+    return users
+}
+/*
 getAllUsers()
 .then(users => {
     const usersRoot = document.getElementById("users-root");
     users.forEach(user => {
+        let row = document.createElement('div')
+        row.classList.add('row')
+        row.innerHTML = user.name
+        usersRoot.appendChild(row)
     });
-})
+}) */
